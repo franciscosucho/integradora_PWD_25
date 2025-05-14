0133f375c305d3be4921db916dbd9dc95c961ac4
franciscosucho <franciscosuchomela@gmail.com>
2025-05-14 20:45:24 -0300

    para entregar

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let result = document.getElementById('result'); // Obtiene el elemento input con id 'result'
-var key_sound = new Audio('key.mp3');
+var key_sound = new Audio('key.mp3'); // Linkenado el archivo .mp3
 
 function calculate(value) {
-    key_sound.play();
+    key_sound.play(); //Hace sonar un .mp3 cuando se hace click en cualquier boton.
     if (value === 'clear') {
         result.value = ''; // Limpia el valor del input
     } 
+
     else if (value === 'calculate') {
         try {
             result.value = eval(result.value); 
@@ -25,38 +26,45 @@ function calculate(value) {
         try {
             result.value = Math.sqrt(eval(result.value)); 
         } catch (error) {
-            result.value = 'Error';
+            result.value = 'Error'; // Maneja errores
         }
     } 
      //calcula sin
     else if (value === 'Math.sin') {
         try {
-            result.value = Math.sin(eval(result.value)); 
+            result.value = Math.sin(eval(result.value)); //realiza la operacion de seno
         } catch (error) {
-            result.value = 'Error';
+            result.value = 'Error'; // Maneja errores
         }
     }
      //calcula cos
     else if (value === 'Math.cos') {
         try {
-            result.value = Math.cos(eval(result.value)); 
+            result.value = Math.cos(eval(result.value)); //realiza la operacion de coseno
         } catch (error) {
-            result.value = 'Error';
+            result.value = 'Error'; // Maneja errores
         }
     }
      //calcula y
     else if (value === 'Math.tan') {
         try {
-            result.value = Math.tan(eval(result.value)); 
+            result.value = Math.tan(eval(result.value)); //realiza la operacion de tangente
         } catch (error) {
-            result.value = 'Error';
+            result.value = 'Error'; // Maneja errores
         }
     }
     else if (value === 'Math.log') {
         try {
-            result.value = Math.log(eval(result.value)); 
+            result.value = Math.log(eval(result.value)); //realiza la operacion de logaritmo
         } catch (error) {
-            result.value = 'Error';
+            result.value = 'Error'; // Maneja errores
+        }
+    }
+    else if (value === 'Math.PI') {
+        try {
+            result.value = Math.PI; //realiza la operacion de logaritmo
+        } catch (error) {
+            result.value = 'Error'; // Maneja errores
         }
     }
     else if (value === '**2') {
@@ -74,8 +82,6 @@ function calculate(value) {
         }
     } 
     else {
-        result.value += value; // Añade el valor del botón al input
+        result.value += value; // Añade el valor total al input
     }
 }
-
-
